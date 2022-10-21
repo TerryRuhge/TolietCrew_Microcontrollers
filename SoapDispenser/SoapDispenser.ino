d9c45315fdcd2cbb9ce9b27c8a302714b53d67da
Terry Ruhge <terry.ruhge@gmail.com>
2022-10-21 17:47:24 -0500

    Detection on Dispense

--- SoapDispenser/SoapDispenser.ino
+++ SoapDispenser/SoapDispenser.ino
@@ -1,10 +1,19 @@
+const byte interruptPin = 2; //Pin Connected to Soap Dispensor motor
+volatile byte state = LOW;
 void setup() {
-  // put your setup code here, to run once:
+  // Set up Printing
   Serial.begin(115200);
+  // Set up interrupt Pin
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), dispensed, FALLING);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("This is a test Print");
-  delay(2000);
+  //Serial.println(state);
+}
+
+void dispensed() {
+  state = !state;
+  Serial.println("Detected!");
 }
